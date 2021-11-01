069dd3f3d1541765dad4072d89840638650b35ea
jhLim97 <wnsgur2749@naver.com>
2021-11-02 00:52:53 +0900

    단순 query를 통한 graphql 통신 테스트

--- apollo-client/src/App.js
+++ apollo-client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const client = new ApolloClient({
+    uri: "https://48p1r2roz4.sse.codesandbox.io",
+    cache: new InMemoryCache(),
+  });
+
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          query GetRates {
+            rates(currency: "USD") {
+              currency
+            }
+          }
+        `,
+      })
+      .then((result) => console.log(result));
+  }, [client]);
+
+  return <div className="App">하이</div>;
 }
 
 export default App;
